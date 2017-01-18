d0e730d6538cedf90f571ebf3a643b988df5d404
Yukio Shimonohara <yshimonohara@cv-lab.com>
2017-01-18 22:55:17 +0900

    StyleCop対応

--- CSharpSample/Program.cs
+++ CSharpSample/Program.cs
@@ -7,11 +7,13 @@
 namespace CSharpSample
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using DesignPattern.Factory;
 
     /// <summary>
     /// Begging of Assembly
     /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "For Japanese support")]
     public class Program
     {
         /// <summary>
@@ -37,30 +39,38 @@ namespace CSharpSample
                 var sample = SampleFactory.Create(sectionNo: sectionNo);
                 sample.Do(exampleNo: exampleNo);
 
-                // Linq to Objectサンプル
-                // TODO;引数で動作を変更するようにする
-                // X. Linq to Object(Linqを取り扱っている章は、4.3)
-                // LinqSampleBehavior001.SampleData001_BasicBehaviorOfLinq();
-
-                //// コマンドパターンサンプル
-                // var receiver = new ConcreateReceiver();
-                // var invoker = new Invoker();
-                // var commands = new ICVLCommand[5];
-                // for (int i = 0; i < commands.Length; i++)
-                // {
-                //    commands[i] = new ConcreateCommandA(i);
-                //    commands[i].SetReciever(receiver);
-                //    invoker.AddCommand(commands[i]);
-                // }
- 
-                // invoker.Execute();
-                // invoker.UndoCommand();
-                // invoker.Execute();
+                // Temporary Sample Code
+                TmpTestCode();
             }
             catch
             {
                 Console.WriteLine("Program Unsuccessfuly Finished");
             }
+        }
+
+        /// <summary>
+        /// 一時的なサンプルコード（いづれ削除）
+        /// </summary>
+        private static void TmpTestCode()
+        {
+            // Linq to Objectサンプル
+            // X. Linq to Object(Linqを取り扱っている章は、4.3)
+            // LinqSampleBehavior001.SampleData001_BasicBehaviorOfLinq();
+
+            //// コマンドパターンサンプル
+            // var receiver = new ConcreateReceiver();
+            // var invoker = new Invoker();
+            // var commands = new ICVLCommand[5];
+            // for (int i = 0; i < commands.Length; i++)
+            // {
+            //    commands[i] = new ConcreateCommandA(i);
+            //    commands[i].SetReciever(receiver);
+            //    invoker.AddCommand(commands[i]);
+            // }
+
+            // invoker.Execute();
+            // invoker.UndoCommand();
+            // invoker.Execute();
         }
     }
 }
