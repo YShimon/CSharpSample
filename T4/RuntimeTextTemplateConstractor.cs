66a18442e064be2d02449cd35d8ba33c629a4fc9
Yukio Shimonohara <yshimonohara@cv-lab.com>
2020-03-10 22:59:32 +0900

    T4 Code Generatorサンプルの追加

---
+++ T4/RuntimeTextTemplateConstractor.cs
@@ -0,0 +1,22 @@
+﻿namespace T4
+{
+    /// <summary>
+    /// ランタイムコード生成クラスにコンテキストを追加するための定義クラス
+    /// </summary>
+    public partial class RuntimeTextTemplate
+    {
+        /// <summary>
+        /// コード生成パラメータ
+        /// </summary>
+        private GenerationContext Context { get; }
+
+        /// <summary>
+        /// コンストラクタ(コンテキスト受取用)
+        /// </summary>
+        /// <param name="context">コンテキスト(生成パラメータ)</param>
+        public RuntimeTextTemplate(GenerationContext context)
+        {
+            Context = context;
+        }
+    }
+}
